d642cbe6c37a5ef559e2db0a2776ba801bf59577
Casper <pnfcre@gmail.com>
2019-08-17 11:58:38 +0200

    :bug: Fixing a bug

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
     siteUrl: `https://zeiw.me`,
     siteDescription: `Open source community-based game 🏓`,
     siteLanguage: `en`,
-    siteImage: `/img/cover.jpg`,
+    siteImage: `/cover.jpg`,
     author: `@zeiwhq`,
   },
   plugins: [
@@ -30,12 +30,12 @@ module.exports = {
         display: `standalone`,
         icons: [
           {
-            src: `/favicon/android-chrome-192x192.png`,
+            src: `/android-chrome-192x192.png`,
             sizes: `192x192`,
             type: `image/png`,
           },
           {
-            src: `/favicon/android-chrome-512x512.png`,
+            src: `/android-chrome-512x512.png`,
             sizes: `512x512`,
             type: `image/png`,
           },
