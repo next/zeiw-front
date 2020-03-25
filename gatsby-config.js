932019233c1da00d7a16c1d3ae3e63f5fbf17a22
Hampus Kraft <pnfcre@gmail.com>
2020-03-25 18:21:42 +0100

    Cleaning up

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,12 +7,12 @@ module.exports = {
     siteHeadline: `ZEIW`,
     siteImage: `/cover.jpg`,
     siteUrl: `https://zeiw.pnfc.re`,
-    siteDescription: `Open source community-based game.`
+    siteDescription: `Open source community-based game.`,
   },
   plugins: [
     {
       resolve: `@lekoarts/gatsby-theme-cara`,
-      options: {}
+      options: {},
     },
     {
       resolve: `gatsby-plugin-manifest`,
@@ -28,15 +28,15 @@ module.exports = {
           {
             src: `/android-chrome-192x192.png`,
             sizes: `192x192`,
-            type: `image/png`
+            type: `image/png`,
           },
           {
             src: `/android-chrome-512x512.png`,
             sizes: `512x512`,
-            type: `image/png`
-          }
-        ]
-      }
+            type: `image/png`,
+          },
+        ],
+      },
     },
     `gatsby-plugin-offline`,
     {
@@ -50,10 +50,10 @@ module.exports = {
             'Referrer-Policy: strict-origin-when-cross-origin',
             'Strict-Transport-Security: max-age=31536000; includeSubDomains; preload',
             "Feature-Policy: accelerometer 'none'; camera 'none'; geolocation 'none'; gyroscope 'none'; magnetometer 'none'; microphone 'none'; payment 'none'; usb 'none'",
-            "Content-Security-Policy: connect-src 'self' img.lekoarts.de www.netlify.com; default-src 'self'; img-src 'self' img.lekoarts.de www.netlify.com; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'"
-          ]
-        }
-      }
-    }
-  ]
+            "Content-Security-Policy: connect-src 'self' img.lekoarts.de www.netlify.com; default-src 'self'; img-src 'self' img.lekoarts.de www.netlify.com; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'",
+          ],
+        },
+      },
+    },
+  ],
 }
